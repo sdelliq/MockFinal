c4602fb86fb6ee0205d9ef2fe17e796d1e98189c
Sophia Dell'Arciprete <sophia.dellarciprete@iqera.it>
2023-11-23 17:30:40 +0100

    we need to start creating our reports

---
+++ ourReports.R
@@ -0,0 +1,12 @@
+#Conventions:
+  #All report tables start with r.
+  #The different words in the name are to be sepparated by a dot
+  #All
+
+# Totali: N borrowers, N loans ,  GBV total, mean GBV (by borr and loan)
+r.totals.loans.borrowers <- as.data.frame(
+  n.borrowers 
+)
+  
+  
+# Totali2: N agreement , N PPT, gbv agreed and gbv paid
